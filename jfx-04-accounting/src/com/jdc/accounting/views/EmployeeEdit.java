18bc415f1a57763f1b669c33151aa5d39d0a66c7
minlwin <lwin.zawmin@gmail.com>
2020-02-08 17:58:11 +0630

    Commit Project

--- jfx-04-accounting/src/com/jdc/accounting/views/EmployeeEdit.java
+++ jfx-04-accounting/src/com/jdc/accounting/views/EmployeeEdit.java
@@ -1,5 +1,62 @@
 package com.jdc.accounting.views;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 public class EmployeeEdit {
+
+    @FXML
+    private Label message;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField code;
+
+    @FXML
+    private ComboBox<?> role;
+
+    @FXML
+    private TextField phone;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextArea address;
+
+    @FXML
+    private void close() {
+
+    }
+
+    @FXML
+    private void save() {
+
+    }
+
+	public static void show() {
+
+		try {
+			Stage stage = new Stage();
+			stage.initModality(Modality.APPLICATION_MODAL);
+			
+			FXMLLoader loader = new FXMLLoader(EmployeeEdit.class.getResource("EmployeeEdit.fxml"));
+			stage.setScene(new Scene(loader.load()));
+			
+			stage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- jfx-04-accounting/src/com/jdc/accounting/views/EmployeeManagement.java
+++ jfx-04-accounting/src/com/jdc/accounting/views/EmployeeManagement.java
@@ -1,5 +1,46 @@
 package com.jdc.accounting.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class EmployeeManagement {
+
+    @FXML
+    private ComboBox<?> schRole;
+
+    @FXML
+    private TextField schCode;
+
+    @FXML
+    private TextField schName;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private void addNew() {
+    	EmployeeEdit.show();
+    }
+
+    @FXML
+    private void clear() {
+    	schRole.setValue(null);
+    	schCode.clear();
+    	schName.clear();
+    	table.getItems().clear();
+    }
+
+    @FXML
+    private void search() {
+    	// get search fields
+    	
+    	// search from database with search fields
+    	
+    	// clear table items
+    	
+    	// add search result to table
+    }
 
 }
